2e2f7a3a0471c3276f21fc2b83012706848b1faf
FranciscoMaier98 <chicosm@outlook.com>
2021-02-25 22:37:31 -0300

    Commit 2 - Criação do banco de dados
    Foi adicionado o banco de dados

---
+++ ia.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2021 at 02:36 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genero`
+--
+
+CREATE TABLE `genero` (
+  `id_genero` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `livro`
+--
+
+CREATE TABLE `livro` (
+  `id_livro` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `descricao` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `livro_genero`
+--
+
+CREATE TABLE `livro_genero` (
+  `id_livro` int(11) NOT NULL,
+  `id_genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`id_genero`);
+
+--
+-- Indexes for table `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`id_livro`);
+
+--
+-- Indexes for table `livro_genero`
+--
+ALTER TABLE `livro_genero`
+  ADD PRIMARY KEY (`id_livro`),
+  ADD KEY `id_genero` (`id_genero`),
+  ADD KEY `id_livro` (`id_livro`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
